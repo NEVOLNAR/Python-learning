e184cacf0f04ebd446006d1330e9db70de0defef
NEVOLNAR <artemnevolnyk@gmail.com>
2021-05-08 14:02:48 +0300

    Was added module Second step.py and was added Multiply function to the module First step

--- First step.py
+++ First step.py
@@ -1,13 +1,20 @@
 def plus(a, b):
     return a + b
 
+
 def minus(a, b):
     return a - b
 
+
+def mul(a, b):
+    return a * b
+
+
 rP = 'result plus ='
 rM = 'result minus ='
+rMu = 'result mutiply = '
 
 a = int(input())
 b = int(input())
 
-print(rP, plus(a, b), 'and', rM, minus(a, b))
+print(rP, plus(a, b), 'and', rM, minus(a, b), 'and', rMu, mul(a, b))
